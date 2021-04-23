8e0409abd19b49ecc0f8d79e671a33e5bf330b8b
Tony Scofield <40931917+ScoTonyField@users.noreply.github.com>
2021-04-24 01:48:45 +0800

    post message and delete

--- Client.py
+++ Client.py
@@ -3,10 +3,10 @@ import sys
 import time
 
 class Client:
-    def __init__(self, host='localhost', port=8888):
+    def __init__(self, host='localhost', port=8888, udp_port=4000):
         print('Try connecting...')
         self.udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.udp_port = self.udp.getsockname()[1]
+        self.udp_port = udp_port
         print(self.udp.getsockname())
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((host, port))
@@ -27,15 +27,27 @@ class Client:
             message = self.sock.recv(1024)
             if message.decode() == '0':
                 self.login = True
+                self.username = username
                 print(response_dict['0'])
-                print(response_dict['3'])
-                cmd = input()
             elif message.decode() == '1':
                 print(response_dict['1'])
             else:
                 print(response_dict['2'])
                 break
+        # Commands operation
+        while self.login:
+            print(response_dict['3'])
+            cmd = input()
+            self.sock.send(cmd.encode())
+            sendBackMsg = self.sock.recv(1024)
+            print(sendBackMsg.decode())
 
+    def logOut(self):
+        if self.login:
+            self.sock.send('logout'.encode())
+            print('Bye! {}'.format(self.username))
+            self.login = False
+            self.status = False
 
 if __name__ == '__main__':
     c = Client()
--- Server.py
+++ Server.py
@@ -1,6 +1,9 @@
 import socket
 import sys
 import time
+import threading
+import re
+
 
 class Server:
     def __init__(self, host, port, failed_num):
@@ -9,82 +12,163 @@ class Server:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((host, port))
         self.sock.listen()
-        self.username = ''
-        self.password = ''
-        self.udp_port = 0
         self.login_log = {}
-        self.failed_num = failed_num
+        self.msg_log = {}
         self.block = {}
         print('Server established...')
         while True:
-            self.client, self.address = self.sock.accept()
-            print('Connection with {} successfully'.format(self.address))
-            self.tcpConnect()
-            self.login = False
+            client, address = self.sock.accept()
+            print('Connection with {} successfully'.format(address))
+            thread = threading.Thread(target=self.tcpConnect, args=(client, address, failed_num))
+            thread.start()
+            # self.login = False
 
-    def tcpConnect(self):
+    def tcpConnect(self, client, address, failed_num):
+        self.updateUserLog()
+        login = False
         cnt = 0
-        while not self.login:
-            message = self.client.recv(1024)
-            self.username = message.decode().split(',')[0]
-            self.password = message.decode().split(',')[1]
-            self.udp_port = message.decode().split(',')[2]
-            if self.isBlock():
+        while not login:
+            message = client.recv(1024)
+            username = message.decode().split(',')[0]
+            password = message.decode().split(',')[1]
+            udp_port = message.decode().split(',')[2]
+            if self.isBlock(address):
                 # self.client.send('Your account is blocked due to multiple login failures. Please try again later')
-                self.client.send('2'.encode())
+                client.send('2'.encode())
                 break
 
-            if self.isValidLogin():
-                print('{} Login'.format(self.username))
-                self.client.send('0'.encode())  # 0 stands for login successfully
-                self.login = True
-                self.login_log[self.username] = time.strftime('%d %b %Y %H:%M:%S') + ',' + self.udp_port
-                self.updateUserLog()
-            elif cnt < self.failed_num:
-                self.client.send('1'.encode())  # 1 stands for login unsuccessfully but has not been block
+            if self.isValidLogin(username, password):
+                print('{} Login'.format(username))
+                client.send('0'.encode())  # 0 stands for login successfully
+                login = True
+                self.login_log[username] = time.strftime('%d %b %Y %H:%M:%S') + ',' + udp_port
+                self.updateUserLog(address)
+            elif cnt < failed_num:
+                print(username)
+                client.send('1'.encode())  # 1 stands for login unsuccessfully but has not been block
                 cnt += 1
             else:
-                self.client.send('2'.encode())  # 2 stands for this IP has been blocked for 10sec
-                self.block[self.address[0]] = time.time()
+                client.send('2'.encode())  # 2 stands for this IP has been blocked for 10sec
+                self.block[address[0]] = time.time()
                 break
+        while login:
+            afterLoginMsg = client.recv(1024)
+            if afterLoginMsg.decode() == 'logout':
+                login = False
+                client.close()
+            elif 'MSG ' in afterLoginMsg.decode():
+                # TODO: empty message and validation
+                # print(afterLoginMsg.decode()[4:])
+                msg = afterLoginMsg.decode()[4:]
+                postTime = time.strftime('%d %b %Y %H:%M:%S')
+                msgId = postTime + '|' + username
+                self.msg_log[msgId] = [msg, 0]
+                self.updateMsgLog()
+                msgIndex = list(self.msg_log).index(msgId) + 1
+                client.send('Message #{} posted at {}.'.format(msgIndex, postTime).encode())
+                print('{} posted MSG #{} "{}" at {}'.format(username, msgIndex, msg, postTime))
+            elif 'DLT ' in afterLoginMsg.decode():
+                pattern = re.compile(r'#\d+')
+                valid, validMsg = self.isDLTValid(afterLoginMsg.decode(), username)
+                if valid:
+                    msgIndex = pattern.findall(afterLoginMsg.decode())[0]
+                    givenTime = afterLoginMsg.decode()[afterLoginMsg.decode().index(msgIndex) + len(msgIndex) + 1:]
+                    givenId = givenTime + '|' + username
+                    deleteMsg = self.msg_log[givenId][0]
+                    self.msg_log.pop(givenId)
+                    self.updateMsgLog()
+                    deleteTime = time.strftime('%d %b %Y %H:%M:%S')
+                    client.send('Message {} deleted at {}'.format(
+                        msgIndex,
+                        deleteTime
+                    ).encode())
+                    print('{} deleted MSG {} "{}" at {}.'.format(
+                        username,
+                        msgIndex,
+                        deleteMsg,
+                        deleteTime
+                    ))
+                else:
+                    client.send(validMsg.encode())
+            else:
+                client.sent('Error. Invalid command!'.encode())
 
-    def isBlock(self):
-        current_time = time.time()
-        if self.address[0] not in self.block:
+    def isDLTValid(self, message, username):
+        pattern = re.compile(r'#\d+')
+        if not pattern.findall(message):
+            return [False, 'Wrong message number, please checked']
+        msgNum = int(pattern.findall(message)[0].split('#')[1])
+        if msgNum > len(self.msg_log):
+            return [False, 'Wrong message number, please checked']
+        else:
+            givenTime = message[message.index(pattern.findall(message)[0]) + len(pattern.findall(message)[0]) + 1:]
+            givenId = givenTime + '|' + username
+            # TODO: givenTime validation
+            if givenId not in self.msg_log:
+                return [False, 'Wrong timestamp or these message not belong to you']
+            else:
+                return [True, '']
+
+
+
+    def isUserMsg(self, number, username):
+        with open('messagelog.txt', 'r') as file:
+            for line in file.readlines():
+                lineNum = line.split(';')[0]
+                name = line.split(';')[2].split(' ')[1]
+                if number == lineNum and username == name:
+                    return True
             return False
-        elif current_time - self.block[self.address[0]] < 10:
+
+
+    def updateMsgLog(self):
+        with open('messagelog.txt', 'w+') as file:
+            for (idx, msgId) in enumerate(self.msg_log):
+                logline = '{}; {}; {}; {}; {}\n'.format(
+                    idx + 1,
+                    msgId.split('|')[0],
+                    msgId.split('|')[1],
+                    self.msg_log[msgId][0],
+                    'yes' if self.msg_log[msgId][1] else 'no'
+                )
+                file.write(logline)
+
+    def isBlock(self, address):
+        current_time = time.time()
+        if address[0] not in self.block:
+            return False
+        elif current_time - self.block[address[0]] < 10:
             return True
         else:
-            del(self.block[self.address[0]])
+            del(self.block[address[0]])
             return False
 
-    def isValidLogin(self):
+    def isValidLogin(self, username, password):
         user_list = []
         with open('credentials.txt', 'r') as file:
             for line in file.readlines():
                 # username, password = line.strip('\n').split(' ')
                 new_line = line.strip('\n')
-                username = new_line.split(' ')[0]
-                password = new_line.split(' ')[1]
-                user_list.append([username, password])
+                defaultUsername = new_line.split(' ')[0]
+                defaultPassword = new_line.split(' ')[1]
+                user_list.append([defaultUsername, defaultPassword])
         for user in user_list:
-            if self.username == user[0] and self.password == user[1]:
+            if username == user[0] and password == user[1]:
                 return True
         return False
 
-    def updateUserLog(self):
-        with open('userlog.txt', 'w+') as logfile:
-            for (idx, name) in enumerate(self.login_log):
-                logline = '{}; {}; {}; {}; {}\n'.format(
-                    idx,
-                    self.login_log[name].split(',')[0],
-                    name,
-                    self.address[0], # TODO: change when mutiple user concerned
-                    self.login_log[name].split(',')[1],
-                )
-                logfile.write(logline)
-
-
+    def updateUserLog(self, *address):
+        if (address):
+            with open('userlog.txt', 'w+') as logfile:
+                for (idx, name) in enumerate(self.login_log):
+                    logline = '{}; {}; {}; {}; {}\n'.format(
+                        idx + 1,
+                        self.login_log[name].split(',')[0],
+                        name,
+                        address[0],
+                        self.login_log[name].split(',')[1],
+                    )
+                    logfile.write(logline)
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
